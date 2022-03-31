761122c14b8615a3e966e44bc2395c2208b7c993
roberto-sanchez06 <sanrobb18@gmail.com>
2022-03-30 18:53:20 -0600

    Iniciando proyecto

---
+++ PruebaAPIS/FrmCurrentTemperature.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace PruebaAPIS
+{
+    partial class FrmCurrentTemperature
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblMaxTemp = new System.Windows.Forms.Label();
+            this.lblMinTemp = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lblPressure = new System.Windows.Forms.Label();
+            this.lblWindSpeed = new System.Windows.Forms.Label();
+            this.lblSunrise = new System.Windows.Forms.Label();
+            this.lblSunset = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblHuminity = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.lblLon = new System.Windows.Forms.Label();
+            this.lblLat = new System.Windows.Forms.Label();
+            this.pbWeather = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWeather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name of city";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(461, 30);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(98, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Search";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Temperature";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Humity";
+            // 
+            // lblMaxTemp
+            // 
+            this.lblMaxTemp.AutoSize = true;
+            this.lblMaxTemp.Location = new System.Drawing.Point(192, 170);
+            this.lblMaxTemp.Name = "lblMaxTemp";
+            this.lblMaxTemp.Size = new System.Drawing.Size(13, 15);
+            this.lblMaxTemp.TabIndex = 6;
+            this.lblMaxTemp.Text = "0";
+            // 
+            // lblMinTemp
+            // 
+            this.lblMinTemp.AutoSize = true;
+            this.lblMinTemp.Location = new System.Drawing.Point(192, 215);
+            this.lblMinTemp.Name = "lblMinTemp";
+            this.lblMinTemp.Size = new System.Drawing.Size(13, 15);
+            this.lblMinTemp.TabIndex = 7;
+            this.lblMinTemp.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 251);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 15);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Pressure";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(393, 96);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 15);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Wind Speed";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(398, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 15);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Sunrise ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(398, 170);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(42, 15);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Sunset";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(398, 204);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 15);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Condition";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(398, 240);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(45, 15);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Details ";
+            // 
+            // lblPressure
+            // 
+            this.lblPressure.AutoSize = true;
+            this.lblPressure.Location = new System.Drawing.Point(112, 251);
+            this.lblPressure.Name = "lblPressure";
+            this.lblPressure.Size = new System.Drawing.Size(29, 15);
+            this.lblPressure.TabIndex = 14;
+            this.lblPressure.Text = "N/A";
+            // 
+            // lblWindSpeed
+            // 
+            this.lblWindSpeed.AutoSize = true;
+            this.lblWindSpeed.Location = new System.Drawing.Point(498, 96);
+            this.lblWindSpeed.Name = "lblWindSpeed";
+            this.lblWindSpeed.Size = new System.Drawing.Size(29, 15);
+            this.lblWindSpeed.TabIndex = 15;
+            this.lblWindSpeed.Text = "N/A";
+            // 
+            // lblSunrise
+            // 
+            this.lblSunrise.AutoSize = true;
+            this.lblSunrise.Location = new System.Drawing.Point(498, 143);
+            this.lblSunrise.Name = "lblSunrise";
+            this.lblSunrise.Size = new System.Drawing.Size(29, 15);
+            this.lblSunrise.TabIndex = 16;
+            this.lblSunrise.Text = "N/A";
+            // 
+            // lblSunset
+            // 
+            this.lblSunset.AutoSize = true;
+            this.lblSunset.Location = new System.Drawing.Point(498, 179);
+            this.lblSunset.Name = "lblSunset";
+            this.lblSunset.Size = new System.Drawing.Size(29, 15);
+            this.lblSunset.TabIndex = 17;
+            this.lblSunset.Text = "N/A";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Location = new System.Drawing.Point(192, 96);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(13, 15);
+            this.lblTemp.TabIndex = 18;
+            this.lblTemp.Text = "0";
+            // 
+            // lblHuminity
+            // 
+            this.lblHuminity.AutoSize = true;
+            this.lblHuminity.Location = new System.Drawing.Point(192, 131);
+            this.lblHuminity.Name = "lblHuminity";
+            this.lblHuminity.Size = new System.Drawing.Size(13, 15);
+            this.lblHuminity.TabIndex = 19;
+            this.lblHuminity.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 15);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Maximum Temperature";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 15);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Minimum Temperature";
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Location = new System.Drawing.Point(608, 33);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(50, 15);
+            this.lblCountry.TabIndex = 22;
+            this.lblCountry.Text = "Country";
+            // 
+            // lblLon
+            // 
+            this.lblLon.AutoSize = true;
+            this.lblLon.Location = new System.Drawing.Point(608, 66);
+            this.lblLon.Name = "lblLon";
+            this.lblLon.Size = new System.Drawing.Size(27, 15);
+            this.lblLon.TabIndex = 23;
+            this.lblLon.Text = "Lon";
+            // 
+            // lblLat
+            // 
+            this.lblLat.AutoSize = true;
+            this.lblLat.Location = new System.Drawing.Point(612, 96);
+            this.lblLat.Name = "lblLat";
+            this.lblLat.Size = new System.Drawing.Size(23, 15);
+            this.lblLat.TabIndex = 24;
+            this.lblLat.Text = "Lat";
+            // 
+            // pbWeather
+            // 
+            this.pbWeather.Location = new System.Drawing.Point(612, 131);
+            this.pbWeather.Name = "pbWeather";
+            this.pbWeather.Size = new System.Drawing.Size(160, 135);
+            this.pbWeather.TabIndex = 25;
+            this.pbWeather.TabStop = false;
+            // 
+            // FrmCurrentTemperature
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbWeather);
+            this.Controls.Add(this.lblLat);
+            this.Controls.Add(this.lblLon);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblHuminity);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.lblSunset);
+            this.Controls.Add(this.lblSunrise);
+            this.Controls.Add(this.lblWindSpeed);
+            this.Controls.Add(this.lblPressure);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblMinTemp);
+            this.Controls.Add(this.lblMaxTemp);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FrmCurrentTemperature";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbWeather)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblMaxTemp;
+        private System.Windows.Forms.Label lblMinTemp;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lblPressure;
+        private System.Windows.Forms.Label lblWindSpeed;
+        private System.Windows.Forms.Label lblSunrise;
+        private System.Windows.Forms.Label lblSunset;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblHuminity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.Label lblLon;
+        private System.Windows.Forms.Label lblLat;
+        private System.Windows.Forms.PictureBox pbWeather;
+    }
+}
---
+++ PruebaAPIS/FrmCurrentTemperature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaAPIS
+{
+    public partial class FrmCurrentTemperature : Form
+    {
+        public FrmCurrentTemperature()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
